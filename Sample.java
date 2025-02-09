e39936a5d607978c5d6994441b04d9e88bb20e5a
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-02-09 05:39:00 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -182,5 +182,58 @@ class Solution {
         }
 
     }
+
 }
 
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    Stack<TreeNode> s= new Stack<>();
+    public BSTIterator(TreeNode root) {
+        dfs(root);
+    }
+    
+    public int next() {
+        TreeNode popped = s.pop();
+        if (popped == null ){
+            System.out.println("what");
+        }
+        if (popped.right != null) {
+            dfs(popped.right);
+        }
+
+        return popped.val;
+    }
+    
+    public boolean hasNext() {
+        return !s.isEmpty();
+    }
+
+    private void dfs(TreeNode root) {
+        while (root != null) {
+            s.push(root);
+            root=root.left;
+        }
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
